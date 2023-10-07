13211fea5ca160015f662193999587dfe1e76c15
Кузнецова Мария Дмитриевна [012751] <012751@pnu.edu.ru>
2023-10-07 21:56:55 +1000

    test preparations

--- 2_testing/src/main/java/com/froggye/ws_23_assignment/JsonParser.java
+++ 2_testing/src/main/java/com/froggye/ws_23_assignment/JsonParser.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
